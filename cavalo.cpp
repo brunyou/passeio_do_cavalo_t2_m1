9900dc4658c5cadb632784e80be194cffea3d26e
Bruno Pereira <bruxeetos@gmail.com>
2023-04-05 08:45:35 -0300

    Create cavalo.cpp

---
+++ cavalo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main (){
+
+
+// criando o tabuleiro
+int tabuleiro[8][8];
+// colocando somente as casas com 0 como populáveis, ou seja não foram passadas ainda
+for (int i; i < 8; i++){
+    for (int j; j < 8; j++){
+        tabuleiro[i][j] = 0;
+    }
+}
+ // criando a função de movimento do cavalo recebendo a posição por parâmtro
+ void mv_cavalo(int pos_l, int pos_c){
+
+ }
+
+return 0;
+}
